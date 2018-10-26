9eb23ed9eec47421a0194299424ec9ff8328862d
Motohiko Makino <motohiko.makino@gmail.com>
2018-10-26 03:07:21 -0700

    initial working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,15 @@ Star Wars RPG Game
 function run() {
   $(".char-box").click(setupFight);
   $("#attack").click(fight);
+  $("#start").hide();
   $("#start").click(start);
 }
 
 let game = {
   player: null, // player's character name
   enemy: null, // enemy's character name
+  attacker: null,
+  defender: null,
   numAttacks: 0, // the number of attacks
 }
 
@@ -23,8 +26,10 @@ let game = {
 // (re-)start the game
 //
 function start() {
-  game.player = "";
-  game.enemy = "";
+  game.player = null;
+  game.enemy = null;
+  game.attacker = null;
+  game.defender = null;
   game.numAttacks = 0;
 
   location.reload();
@@ -54,6 +59,7 @@ function setupFight(event) {
       console.log("Enemy " + $(this).attr("id") + " is selected");
       $(this).addClass("char-box-defender");
       $(this).appendTo("#defender-section");
+      clearMsg();
     }
   }
   // user can move the enemy defender back to the Enemy section
@@ -70,52 +76,102 @@ function setupFight(event) {
 // Character info as a fighter 
 //
 class Fighter {
-  constructor(id, apMax = 10, cApFactor = 2) {
+  constructor(id, apMax = 10, cApFactor = 1.0) {
     this.elem = $(`#${id}`);
     this.name = $(`#${id} h3.char-name`).text();
-    this.hp = $(`#${id} h3.char-hp`).text();
+    this.hp = $(`#${id} h3.char-hp`);
     this.ap = this.attackPower(apMax);
     this.cAp = this.counterAttackPower(cApFactor);
   }
 
-  attackPower(base = 5, max = 15) {
+  attackPower(max = 10, base = 3) {
     return Math.floor(Math.random() * max) + (max - base);
   }
 
-  counterAttackPower(factor = 2.0) {
+  counterAttackPower(factor = 1.0) {
     return Math.floor(this.ap * factor);
   }
 
   outOfHealthPoint() {
-    return (this.hp < 0);
+    return (this.healthPoint <= 0);
+  }
+
+  set healthPoint(point) {
+    this.hp.text(point);
+  }
+
+  get healthPoint() {
+    return parseInt(this.hp.text());
   }
 }
 
-
-let inBattle = {
-  attacker: null,
-  defender: null
-}
 
 //
 // fight as a user clicks on the Attack button 
 //
 function fight() {
-  if (!inBattle.attacker) {
-    inBattle.attacker = new Fighter(game.player);
+  if (game.player && !game.attacker) {
+    game.attacker = new Fighter(game.player, 10, 1.0);
   }
-  if (!inBattle.defender) {
-    inBattle.defender = new Fighter(game.enemy);
+  if (game.enemy && !game.defender) {
+    game.defender = new Fighter(game.enemy, 10, 1.2);
   }
+  if (!game.attacker || !game.defender) {
+    return;
+  }
+
   game.numAttacks++;
+  attack();
+  displayMessage();
 
-  console.log("Attack button is clicked " + game.numAttacks + " times");
-  console.log("player " + inBattle.attacker.name + " " + inBattle.attacker.hp + " " + inBattle.attacker.ap + " " + inBattle.attacker.cAp);
-  console.log("defender " + inBattle.defender.name + " " + inBattle.defender.hp + " " + inBattle.defender.ap + " " + inBattle.defender.cAp);
-
-  if (inBattle.defender.outOfHealthPoint()) {
-    game.enemy.detach();
+  if (game.defender.outOfHealthPoint()) {
+    game.defender.elem.detach();
+    game.defender = null;
     game.enemy = null;
-    game.numAttacks = 0;
   }
+}
+
+//
+// A facilitator function for fight()
+//
+function attack() {
+  ap = game.attacker.ap * game.numAttacks;
+  game.attacker.healthPoint = game.attacker.healthPoint - game.defender.cAp;
+  game.defender.healthPoint = game.defender.healthPoint - ap;
+
+  if (game.attacker.healthPoint < 0) {
+    game.attacker.healthPoint = 0;
+  }
+  if (game.defender.healthPoint < 0) {
+    game.defender.healthPoint = 0;
+  }
+}
+
+function displayMessage() {
+  clearMsg();
+  if (game.attacker.outOfHealthPoint()) {
+    $("#msg1").text("You've been defeated. GAME OVER!!!");
+    $("#start").show();
+  } else if (game.defender.outOfHealthPoint()) {
+    if (remainingEnemies() === 0) {
+      $("#msg1").text("You Won!!! GAME OVER!!!");
+      $("#start").show();
+    } else {
+      $("#msg1").text(`You have defeated ${game.defender.name}. You can choose to finght another enemy.`);
+    }
+  } else {
+    $("#msg1").text(`You attacked ${game.defender.name} for ${ap} damage`);
+    $("#msg2").text(`${game.defender.name} attacked you back for ${game.defender.cAp} damage`);
+  }
+}
+
+function clearMsg() {
+  $("#msg1").text("");
+  $("#msg2").text("");
+}
+// You attacked ${defender} for # damage
+// ${defender} attacked you back for # damage
+
+function remainingEnemies() {
+  return $("#enemy-section > .char-box").length;
 }
